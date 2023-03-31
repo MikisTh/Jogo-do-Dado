d508b6916d1641c2143ca3908db94c779ae2b472
MikisTh <114635629+MikisTh@users.noreply.github.com>
2023-03-31 14:01:02 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,22 +1,74 @@
+
+
+
 var randomNumber1 = Math.floor(Math.random() * 6) + 1; //1-6
+
+switch(randomNumber1){
+	case 1:
+		document.getElementById("dice").src = "dice1.png";
+		break;
+	case 2:
+		document.getElementById("dice").src = "dice2.png";
+		break;
+	case 3:
+		document.getElementById("dice").src = "dice3.png";
+		break;
+	case 4:
+		document.getElementById("dice").src = "dice4.png";
+		break;
+	case 5:
+		document.getElementById("dice").src = "dice5.png";
+		break;
+	case 6:
+		document.getElementById("dice").src = "dice6.png";
+		break;
+	default:
+		//caso contrário
+		break;
+}
 
 var randomDiceImage = "dice" + randomNumber1 + ".png"; //dice1.png - dice6.png
 
-var randomImageSource = "imagens/" + randomDiceImage; //images/dice1.png - images/dice6.png
+var randomImageSource = "images/" + randomDiceImage; //images/dice1.png - images/dice6.png
 
 var image1 = document.querySelectorAll("img")[0];
 
 image1.setAttribute("src", randomImageSource);
 
 
-var randomNumber2 = Math.floor(Math.random() * 6) + 1;
+var randomNumber2 = Math.floor(Math.random() * 6) + 1; //1-6
 
-var randomImageSource2 = "imagens/dice" + randomNumber2 + ".png";
+switch(randomNumber2){
+	case 1:
+		document.getElementById("dice").src = "dice1.png";
+		break;
+	case 2:
+		document.getElementById("dice").src = "dice2.png";
+		break;
+	case 3:
+		document.getElementById("dice").src = "dice3.png";
+		break;
+	case 4:
+		document.getElementById("dice").src = "dice4.png";
+		break;
+	case 5:
+		document.getElementById("dice").src = "dice5.png";
+		break;
+	case 6:
+		document.getElementById("dice").src = "dice6.png";
+		break;
+	default:
+		//caso contrário
+		break;
+}
+
+var randomImageSource2 = "images/dice" + randomNumber2 + ".png";
 
 document.querySelectorAll("img")[1].setAttribute("src", randomImageSource2);
 
 
 //If player 1 wins
+
 if (randomNumber1 > randomNumber2) {
   document.querySelector("h1").innerHTML = "O Jogador 1 Venceu! 🏆";
 }
